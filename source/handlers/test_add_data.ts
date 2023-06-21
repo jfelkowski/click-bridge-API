51300d3774370dda8c15de1e09e9f4674aa4a0e7
Jeff Felkowski <111080091+jfelkowski@users.noreply.github.com>
2023-06-21 17:39:39 -0400

    add test route, add data to db

---
+++ source/handlers/test_add_data.ts
@@ -0,0 +1,19 @@
+import prisma from "../db";
+
+export const testAddData = async (req, res, next) => {
+  try {
+    const testAddData = await prisma.clickstream.create({
+      data: {
+        article: 'Albert Einstein',
+        referral_article: 'Theory of Relativity',
+        count: 7894,
+        type: 'link',
+        year: '2023',
+        month: '03',
+     },
+    });
+    res.json({data: testAddData});
+  } catch (e) {
+    next(e);
+  }  
+};
--- source/router.ts
+++ source/router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { getSearchResults } from './handlers/clickstream';
+import { testAddData } from './handlers/test_add_data';
 
 const router = Router();
 
 router.get('/search', getSearchResults);
+
+router.post('/test', testAddData);
 
 router.use((err, req, res, next) => {
   console.log(err);
