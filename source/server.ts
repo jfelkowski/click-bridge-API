37864fbe7d3ad90b6a800f9e91ca17865750e6b8
Jeff Felkowski <111080091+jfelkowski@users.noreply.github.com>
2023-06-20 20:57:11 -0400

    add morgan request logger

--- source/server.ts
+++ source/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
